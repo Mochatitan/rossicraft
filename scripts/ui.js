ac88f6c27967e3da9ab733ba69c801350416abf7
Mochatitan <mochatitanstudios@gmail.com>
2024-04-18 17:40:18 -0400

    Added Genuine Terrain
    Added Seeds
    made Render not load blocks not visible

--- scripts/ui.js
+++ scripts/ui.js
@@ -5,6 +5,19 @@ export function createUI(world){
 
     gui.add(world.size, 'width', 8 , 128, 1).name("Width"); //object is world.size, the thing changing is width, the minimum is 8, maximum is 128, step one at a time,
     gui.add(world.size, 'height', 8 , 128, 1).name("Height");
+    
+    const terrainFolder = gui.addFolder('Terrain');
+
+    terrainFolder.add(world.params, 'seed', 0, 1000).name("World Seed");
+    
+    terrainFolder.add(world.params.terrain, 'scale', 8, 64, 1).name("Scale");
+    terrainFolder.add(world.params.terrain, 'magnitude', 0, 1, 0.02).name("Magnitude");
+    terrainFolder.add(world.params.terrain, 'offset', 0, 1, 0.02).name("Offset");
+    
+
+    
+
+    
     //gui.add(world, 'generate');
 
     gui.onChange(() => {
--- scripts/world.js
+++ scripts/world.js
@@ -1,8 +1,11 @@
 import * as THREE from 'three';
+import { SimplexNoise } from 'three/examples/jsm/math/SimplexNoise.js';
+import { RNG } from './rng';
+import { blocks } from './blocks';
 
 // Create Cube
 const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshLambertMaterial({ color: 0x00d000 }); //Lambert has shading, basic material does not
+const material = new THREE.MeshLambertMaterial(); //Lambert has shading, basic material does not
 
 export class World extends THREE.Group {
     /**
@@ -13,17 +16,34 @@ export class World extends THREE.Group {
      */
     data = [];
 
+    params = {
+        seed: 0,
+        terrain : {
+            scale: 30,
+            magnitude: 0.5,
+            offset: 0.2
+        },
+
+    };
+        
+    threshold = 0.5;
+
     constructor(size = {width:64, height: 32}) { //if no size is passed, default will be 32
         super();
         this.size = size;
     }
 
     generate(){
-
+        this.initializeTerrain();
+        this.generateTerrain();
+        this.generateMeshes();
 
     }
 
-    generateTerrain(){
+    /**
+     * initializing the world terrain data
+     */
+    initializeTerrain(){
         this.data = []; //clear data array
         for (let x = 0; x < this.size.width; x++){
             const slice = [];
@@ -31,7 +51,7 @@ export class World extends THREE.Group {
                 const row = [];
                 for (let z = 0; z < this.size.width; z++){
                     row.push({
-                        id: 1,
+                        id: blocks.empty.id,
                         instanceId: null
                     });
             
@@ -43,9 +63,42 @@ export class World extends THREE.Group {
 
     }
 
+    generateTerrain() {
+        const rng = new RNG(this.params.seed);
+        const simplex = new SimplexNoise(rng);
+        for (let x = 0; x < this.size.width; x++) {
+            for (let z = 0; z < this.size.width; z++){
+                const value = simplex.noise(
+                    x / this.params.terrain.scale, 
+                    z / this.params.terrain.scale
+                );
+
+                // Scale the noise based on the magnitude/offset
+                const scaledNoise = this.params.terrain.offset + this.params.terrain.magnitude * value;
+
+                // Computing the height of the terrain at this x-z location
+                let height = Math.floor(this.size.height * scaledNoise);
+
+                // Clamping height between 0 and max height
+                height = Math.max(0, Math.min(height, this.size.height));
+
+                // Fill in all blocks at or below the terrain height
+                for (let y =  0; y <= this.size.height; y++){
+                    if (y < height){
+                        this.setBlockId(x, y, z, blocks.dirt.id);
+                    } else if (y === height){
+                        this.setBlockId(x, y, z, blocks.grass.id);
+                    } else {
+                        this.setBlockId(x, y, z, blocks.empty.id);
+                    }
+                }
+            }
+        }
+    }
+
     generateMeshes(){
         this.clear();
-        //initialize instanced mesh
+
         const maxCount = this.size.width * this.size.width * this.size.height;
         const mesh = new THREE.InstancedMesh(geometry, material,  maxCount);
         mesh.count = 0;
@@ -55,8 +108,17 @@ export class World extends THREE.Group {
         for (let x = 0; x < this.size.width; x++) {
             for(let y = 0; y < this.size.height; y++){
                 for(let z = 0; z < this.size.width; z++){
-                    matrix.setPosition(x + 0.5, y + 0.5, z + 0.5); // +0.5 because the center of the box is at the origin but the lower left corner is now lower left corner of first box.
-                    mesh.setMatrixAt(mesh.count++, matrix);
+                    const blockId = this.getBlock(x, y, z).id;
+                    const blockType = Object.values(blocks).find(x => x.id === blockId); // checks all blocks for an id that fits our block id
+                    const instanceId = mesh.count;
+
+                    if (blockId !== blocks.empty.id && !this.isBlockObscured(x, y, z)) {
+                        matrix.setPosition(x + 0.5, y + 0.5, z + 0.5); // +0.5 because the center of the box is at the origin but the lower left corner is now lower left corner of first box.
+                        mesh.setMatrixAt(instanceId, matrix);
+                        mesh.setColorAt(instanceId, new THREE.Color(blockType.color));
+                        this.setBlockInstanceId(x, y, z, instanceId);
+                        mesh.count++;
+                    }
                 }
             }
         }
@@ -64,10 +126,36 @@ export class World extends THREE.Group {
         this.add(mesh);
     }
 
-
     // Helper Methods
     
     /**
+    * Returns true if this block is completely hidden by other blocks
+   * @param {number} x 
+   * @param {number} y 
+   * @param {number} z 
+   * @returns {boolean}
+   */
+  isBlockObscured(x, y, z) {
+    const up = this.getBlock(x, y + 1, z)?.id ?? blocks.empty.id;
+    const down = this.getBlock(x, y - 1, z)?.id ?? blocks.empty.id;
+    const left = this.getBlock(x + 1, y, z)?.id ?? blocks.empty.id;
+    const right = this.getBlock(x - 1, y, z)?.id ?? blocks.empty.id;
+    const forward = this.getBlock(x, y, z + 1)?.id ?? blocks.empty.id;
+    const back = this.getBlock(x, y, z - 1)?.id ?? blocks.empty.id;
+  
+    // If any of the block's sides is exposed, it is not obscured
+    if (up === blocks.empty.id ||
+        down === blocks.empty.id || 
+        left === blocks.empty.id || 
+        right === blocks.empty.id || 
+        forward === blocks.empty.id || 
+        back === blocks.empty.id) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+     /** 
      *  Gets the block data at (x, y, z)
      * @param {number} x 
      * @param {number} y 
@@ -121,7 +209,6 @@ export class World extends THREE.Group {
         {
             return true;
         } else {
-            alert("block not in bounds");
             return false;
         }
     }
