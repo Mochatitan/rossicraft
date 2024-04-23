7f91de8c8eda748160d477a8fa5f41e4bb340142
Mochatitan <119437575+Mochatitan@users.noreply.github.com>
2024-04-23 08:08:13 -0400

    added audio and images and achievements

---
+++ scripts/achievements.js
@@ -0,0 +1,14 @@
+
+export const achivements = {
+    jumpTen: {
+        id: 0,
+        name: 'Certified Bunny',
+        description: "Jump until you cant jump no more",
+        achieved: false,
+
+
+    }
+
+
+
+}
---
+++ scripts/audio.js
@@ -0,0 +1,15 @@
+export class Music {
+    cold = new Audio("public/audio/music/sweden.mp3");
+
+    playSong(){
+        this.cold.play();
+    }
+}
+
+
+
+
+
+
+
+
